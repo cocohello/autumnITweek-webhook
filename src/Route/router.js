45154dcf4cc5708455ede36f713c0a3a85db02ba
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-29 13:28:55 +0900

    JSON stringify added

--- src/Route/router.js
+++ src/Route/router.js
@@ -16,7 +16,7 @@ const eventController = require('../Controller/eventController');
 	});
 	
 	router.post('/',(req, res) => {
-		console.log(`router.js ${req.body}`);
+		console.log(`router.js ${JSON.stringify(req.body)}`);
 		let response;
 		if (req.queryResult) {			//request from detectWebhookIntent to add followup event
 			let text = 'success to catch webhook request';

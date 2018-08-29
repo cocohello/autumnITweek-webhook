99f54dcaec8e265e297ea7a7b9f884087bab2c45
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-29 22:10:39 +0900

    fsdfsf

--- src/Controller/eventController.js
+++ src/Controller/eventController.js
@@ -4,19 +4,18 @@
 
 const orchestrator = require('../Model/orchestrator');
 
-module.exports = {
-	'work1Process' : work1Process,
-	'work2Process' : work2Process
-}
-	
-const work1Process = function (parameter) {
-	const config = require('../../resource/orchestrator_Environment');
-	const O = orchestrator(config);
-	let response;
-	
-}
+module.exports = evController;
 
-const work2Process = function (parameter) {
-	
-	return;
+const evController = {
+	work1Process : function (parameter) {
+		const config = require('../../resource/orchestrator_Environment');
+		const O = orchestrator(config);
+		let response;
+		return;
+	},
+	work2Process : function (parameter) {
+		
+		return;
+	}
 }
+	

8334465496778a3238fc53c4e1f9462faf54cd77
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-30 08:43:27 +0900

    dddd

--- src/Controller/eventController.js
+++ src/Controller/eventController.js
@@ -15,8 +15,8 @@ const evController = {
 		var arr = outputContexts.fields;
 		for(let context in arr){
 			//console.log(arr[context]);
-			console.log(arr[2].structValue.fields.name.parameters);
-			console.log(structjson.structProtoToJson(arr[2].structValue.fields.name.parameters));
+			console.log(arr[2].structValue.fields);
+			console.log(structjson.structProtoToJson(arr[2].structValue.fields));
 		}
 		/*var context;
 		(outputContexts.fields).forEach(function (item, index, arr) {

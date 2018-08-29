5aba2ce8ac2819bbe6526713bde61f1df986a647
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-30 08:05:37 +0900

    ss

--- src/Route/router.js
+++ src/Route/router.js
@@ -46,8 +46,9 @@ const structjson = require('../Util/structjson');
 			
 			let detectedEvent = req.body.queryResult.queryText;
 			switch (detectedEvent) {
-				case 'work1_process_event' : 
-					eventController.work1Process(structjson.jsonToStructProto(req.body.queryResult.outputContexts));
+				case 'work1_process_event' :
+					console.log(structjson.jsonToStructProto(req.body.queryResult));
+					//eventController.work1Process();
 					break;
 				case 'work2_process_event' :
 					eventController.work2Process(req.queryInput);

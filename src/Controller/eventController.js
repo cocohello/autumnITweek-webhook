2a06ca4993104f5df65609bc010f09257c3e6430
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-29 13:05:35 +0900

    model update

--- src/Controller/eventController.js
+++ src/Controller/eventController.js
@@ -2,11 +2,11 @@
  * http://usejsdoc.org/
  */
 
-const orchestrator = require('./Model/orchestrator');
+const orchestrator = require('../Model/orchestrator');
 
 module.exports = function() {
 	const middleware = {};
-	const config = require('../resource/orchestrator_Environment');
+	const config = require('../../resource/orchestrator_Environment');
 	const O = orchestrator(config);
 	let response;
 	middleware.work1Process = (queryInput) => {

6f56eb8e3929838d192725418baa3b3876500483
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-30 16:50:37 +0900

    fsdfdsf

--- src/Controller/eventController.js
+++ src/Controller/eventController.js
@@ -12,58 +12,52 @@ const O = orchestrator({
 });
 
 //sign in to Orchestrator and get token
-
-let token;
-O.login().then(result => {
-	console.log('then'+result);
-	token = result;
-});
-console.log('eve'+token);
-const evController = {
-	work1Process : function (outputContexts) {
-		//get the receipt folder path from request query
-		const arr = structjson.structProtoToJson(outputContexts);
-		let output;
-		
-		for(let context in arr){
-			let name = arr[context].name;
-			if(name.substr(name.lastIndexOf('/')+1, name.length-1) === 'work1_process_event') { 
-				output = arr[context];
+O.login().then(token => {
+	const evController = {
+		work1Process : function (outputContexts) {
+			//get the receipt folder path from request query
+			const arr = structjson.structProtoToJson(outputContexts);
+			let output;
+			
+			for(let context in arr){
+				let name = arr[context].name;
+				if(name.substr(name.lastIndexOf('/')+1, name.length-1) === 'work1_process_event') { 
+					output = arr[context];
+				}
 			}
+			
+			let dest_path = output['parameters']['0']['dest_path'];
+			console.log(dest_path);
+			//set asset value to the receipt folder path in orchestrator
+			
+			//UI Path//
+			O.getAsset(token, 'work1');
+			
+			//O.putAsset(token, );
+			//const config = require('../../resource/orchestrator_Environment');
+			//const O = orchestrator(config);
+			//let response;
+			/*for(let item in parameter){
+				console.log(item);
+			}
+			var context;
+			(outputContexts.fields).forEach(function (item, index, arr) {
+				//if(outputContexts[index].name) {
+					//context = outputContexts[2];
+					console.log(item.structValue);
+					console.log(JSON.stringify(item.structValue));
+				//}
+			})*/
+			
+			
+			
+			return;
+		},
+		work2Process : function (parameter) {
+			
+			return;
 		}
-		
-		let dest_path = output['parameters']['0']['dest_path'];
-		console.log(dest_path);
-		//set asset value to the receipt folder path in orchestrator
-		
-		//UI Path//
-		O.getAsset(token, 'work1');
-		
-		//O.putAsset(token, );
-		//const config = require('../../resource/orchestrator_Environment');
-		//const O = orchestrator(config);
-		//let response;
-		/*for(let item in parameter){
-			console.log(item);
-		}
-		var context;
-		(outputContexts.fields).forEach(function (item, index, arr) {
-			//if(outputContexts[index].name) {
-				//context = outputContexts[2];
-				console.log(item.structValue);
-				console.log(JSON.stringify(item.structValue));
-			//}
-		})*/
-		
-		
-		
-		return;
-	},
-	work2Process : function (parameter) {
-		
-		return;
 	}
-}
-	
+});
 
 module.exports = evController;

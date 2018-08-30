d26bb902dc0ad81eb60a7e07f3c697caf0b0b2ca
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-30 09:18:15 +0900

    ddd

--- src/Controller/eventController.js
+++ src/Controller/eventController.js
@@ -13,10 +13,17 @@ const evController = {
 		//.lastIndexOf('/')
 		//const dess_path = structjson.structProtoToJson();
 		var arr = outputContexts.fields;
+		var parameter;
 		for(let context in arr){
 			//console.log(arr[context]);
-			console.log(arr[context].structValue.fields.parameters);
+			if(arr[context].name.stringValue === 'projects/autumn-it-week-2018/agent/sessions/7d0ec78ea1139d0a6d12170a9f32d323/contexts/work1_process_event'){
+				parameter = arr[context].structValue.fields.parameters;
+			}
 			//console.log(structjson.structProtoToJson(arr[2].structValue.fields.parameters));
+		}
+		console.log(parameter.structValue);
+		for(let item in parameter){
+			console.log(item);
 		}
 		/*var context;
 		(outputContexts.fields).forEach(function (item, index, arr) {

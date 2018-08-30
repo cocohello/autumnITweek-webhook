10d047673a7cc94ffeb44e673c65aefbb3b25b8e
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-30 13:21:16 +0900

    fsdfs

--- src/Controller/eventController.js
+++ src/Controller/eventController.js
@@ -21,17 +21,17 @@ const evController = {
 		//const O = orchestrator(config);
 		//let response;
 		//.lastIndexOf('/')
-		//const dess_path = structjson.structProtoToJson();
-		var arr = outputContexts.fields;
-		var parameter = structjson.structProtoToJson(outputContexts)['2']['parameters']['0']['dest_path'];
-		console.log(parameter);
+		let dest_path;
+		const arr = structjson.structProtoToJson(outputContexts);
+		//var parameter = structjson.structProtoToJson(outputContexts)['2']['parameters']['0']['dest_path'];
+		
 		for(let context in arr){
-			//console.log(arr[context].name);
-			//if(arr[context].name.stringValue === 'projects/autumn-it-week-2018/agent/sessions/7d0ec78ea1139d0a6d12170a9f32d323/contexts/work1_process_event'){
-				//parameter =structjson.structProtoToJson(arr[2].structValue.fields.parameters);
-			//}
-			//console.log(structjson.structProtoToJson(arr[2].structValue.fields.parameters));
+			if(arr[context].name === 'projects/autumn-it-week-2018/agent/sessions/7d0ec78ea1139d0a6d12170a9f32d323/contexts/work1_process_event'){
+				dest_path = arr[context][parameter]['0']['dest_path'];
+			}
 		}
+		
+		console.log(dest_path);
 		/*for(let item in parameter){
 			console.log(item);
 		}
@@ -43,10 +43,6 @@ const evController = {
 				console.log(JSON.stringify(item.structValue));
 			//}
 		})*/
-		//console.log(JSON.stringify(context));
-		//console.log(structjson.jsonToStructProto(context));
-		
-		
 		
 		
 		

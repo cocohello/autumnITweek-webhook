87ba61975babed6a4f5cbe73e48b1c1f8062feca
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-08-30 09:00:41 +0900

    sss

--- src/Controller/eventController.js
+++ src/Controller/eventController.js
@@ -12,20 +12,21 @@ const evController = {
 		//let response;
 		//.lastIndexOf('/')
 		//const dess_path = structjson.structProtoToJson();
-		var arr = outputContexts.fields;
-		for(let context in arr){
+		var arr = outputContexts.fields.structValue.fields.parameters;
+		/*for(let context in arr){
 			//console.log(arr[context]);
 			console.log(arr[context].structValue.fields.parameters);
 			//console.log(structjson.structProtoToJson(arr[2].structValue.fields.parameters));
-		}
-		/*var context;
-		(outputContexts.fields).forEach(function (item, index, arr) {
+		}*/
+		var context;
+		arr.forEach(function (item, index, arr) {
 			//if(outputContexts[index].name) {
 				//context = outputContexts[2];
-				console.log(item.structValue);
-				console.log(JSON.stringify(item.structValue));
+				console.log(arr[2]);
+				console.log('\n');
+				console.log(arr[2].fields['0']);
 			//}
-		})*/
+		})
 		//console.log(JSON.stringify(context));
 		//console.log(structjson.jsonToStructProto(context));
 		return;

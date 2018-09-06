ff5df79fb0c4bb5bcefd22e153801fb948386293
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-09-06 11:48:14 +0900

    d

--- src/Route/router.js
+++ src/Route/router.js
@@ -71,7 +71,7 @@ let resJob;
 					
 					break;
 				case 'work2_process_event' :
-					console.log(JSON.stringify(req.body);
+					console.log(JSON.stringify(req.body));
 					/*var result = eventController.work2Process(structjson.jsonToStructProto(req.body.queryResult.outputContexts))
 					result.then(result => {
 						if(typeof result === 'string'){

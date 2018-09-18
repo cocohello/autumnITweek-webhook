6151ff09e323612a9466fe5134aa98b8530a8656
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-09-18 14:44:36 +0900

    fsd

--- src/Controller/eventController.js
+++ src/Controller/eventController.js
@@ -55,7 +55,7 @@ const evController = {
 					return O.putAsset(result, assetProperties).then( result => {
 						console.log(2);
 						console.log(result);
-						return O.getReleaseId('register').then( result => {
+						return O.getReleaseId('module1').then( result => {
 							console.log(3);
 							console.log(result);
 							return O.startJob(result).then( result => {
@@ -103,7 +103,7 @@ const evController = {
 					return O.putAsset(result, assetProperties).then( result => {
 						console.log(2);
 						console.log(result);
-						return O.getReleaseId('assetTest').then( result => {
+						return O.getReleaseId('module2').then( result => {
 							console.log(3);
 							console.log(result);
 							return O.startJob(result).then( result => {

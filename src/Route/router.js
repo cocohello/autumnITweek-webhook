86630cd9edf43023649a50eb4cb97a26c8635efb
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-09-18 16:36:49 +0900

    d

--- src/Route/router.js
+++ src/Route/router.js
@@ -79,7 +79,7 @@ let resJob;
 	});
 	
 	
-	/*router.get('/work', (req, res) => {
+	router.get('/work', (req, res) => {
 		console.log(`router.js from orchestrator ${JSON.stringify(req.body.jobId)} \n`);
 		let response = {};
 		res.json(response);
@@ -132,7 +132,7 @@ let resJob;
 		res.setHeader('Content-Type', 'application/json');  
 		res.send(JSON.stringify(response));
 	})
-*/	
+	
 	
 module.exports = router;
 

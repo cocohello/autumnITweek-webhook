e59aa83e9d5fdae1952c55d7748f997c2214dc02
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-09-19 17:30:39 +0900

    xx

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(function (req, res) {
 	console.log('come??');
 	  var delayed = new DelayedResponse(req, res);
 	  // verySlowFunction can now run indefinitely
-	  verySlowFunction(delayed.start());
+	  delayed.start();
 	});
 
 

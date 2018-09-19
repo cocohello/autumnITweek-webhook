80d84e3293fadf11436b00f488e38f8d2908cdfd
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-09-19 16:12:12 +0900

    ss

--- index.js
+++ index.js
@@ -21,12 +21,69 @@ app.use(express.static(path.join(__dirname)));
 const route = require('./src/Route/router');
 app.use('/', route);
 
+const extendTimeoutMiddleware = (req, res, next) => {
+	  const space = ' ';
+	  let isFinished = false;
+	  let isDataSent = false;
+
+	  // Only extend the timeout for API requests
+	  if (!req.url.includes('/work_result')) {
+	    next();
+	    return;
+	  }
+
+	  res.once('finish', () => {
+	    isFinished = true;
+	  });
+
+	  res.once('end', () => {
+	    isFinished = true;
+	  });
+
+	  res.once('close', () => {
+	    isFinished = true;
+	  });
+
+	  res.on('data', (data) => {
+	    // Look for something other than our blank space to indicate that real
+	    // data is now being sent back to the client.
+	    if (data !== space) {
+	      isDataSent = true;
+	    }
+	  });
+
+	  const waitAndSend = () => {
+	    setTimeout(() => {
+	      // If the response hasn't finished and hasn't sent any data back....
+	      if (!isFinished && !isDataSent) {
+	        // Need to write the status code/headers if they haven't been sent yet.
+	        if (!res.headersSent) {
+	          res.writeHead(202);
+	        }
+
+	        res.write(space);
+
+	        // Wait another 15 seconds
+	        waitAndSend();
+	      }
+	    }, 30000);
+	  };
+
+	  waitAndSend();
+	  next();
+	};
+
+	app.use(extendTimeoutMiddleware);
+
+
+
+
+
 const server = app.listen(port, () => {
 	console.log('bibiServer listening at port %d', port);
 })
 
-server.timeout = 2147483647;
+server.timeout = 0;
 server.on('connection', function(socket) {
-	  socket.setTimeout(2147483647); 
-	  // 30 second timeout. Change this as you see fit.
+	  socket.setTimeout(0); 
 	})

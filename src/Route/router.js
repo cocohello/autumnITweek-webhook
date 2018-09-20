2bfeb1d5133379f10e1693eb9387e64d3978d9b4
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-09-20 11:54:20 +0900

    dfsdfs

--- src/Route/router.js
+++ src/Route/router.js
@@ -129,11 +129,15 @@ let resJob;
 							]
 				}
 			}
-			delayed = new DelayedResponse(req, res);
+			//delayed = new DelayedResponse(req, res);
 			// verySlowFunction can now run indefinitely
-			delayed.start();
+			//delayed.start();
+			
+			extendTimeoutMiddleware = (req, res);
+			
 			resJob = res;
 			flag++;
+			
 		}else{
 			console.log(`router.js from orchestrator ${JSON.stringify(req.body.jobId)} \n`);
 			var a=res;
@@ -147,6 +151,52 @@ let resJob;
 			response = {};
 		}
 	})
+	
+	
+	const extendTimeoutMiddleware = (req, res) => {
+		const space = ' ';
+		let isFinished = false;
+		let isDataSent = false;
+		
+		res.once('finish', () => {
+			isFinished = true;
+		});
+		
+		res.once('end', () => {
+			isFinished = true;
+		});
+		
+		res.once('close', () => {
+			isFinished = true;
+		});
+		
+		res.on('data', (data) => {
+			// Look for something other than our blank space to indicate that real
+			// data is now being sent back to the client.
+			if (data !== space) {
+				isDataSent = true;
+			}
+		});
+		
+		const waitAndSend = () => {
+			setTimeout(() => {
+				// If the response hasn't finished and hasn't sent any data back....
+				if (!isFinished && !isDataSent) {
+					// Need to write the status code/headers if they haven't been sent yet.
+					if (!res.headersSent) {
+						res.writeHead(202);
+					}
+					
+					res.write(space);
+					
+					// Wait another 15 seconds
+					waitAndSend();
+				}
+			}, 20000);
+		};
+		
+		waitAndSend();
+	};
 	
 	
 module.exports = router;

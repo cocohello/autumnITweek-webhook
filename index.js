2578ce185ae3300706465ad6e8ee10edb64b0b35
user_bbn043 <user_bbn043@DESKTOP-CFJP0QA>
2018-09-20 13:09:17 +0900

    dfssf

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname)));
 
-app.use((req, res) => {
+app.use((req, res, next) => {
 		// Only extend the timeout for API requests
 	  if (!req.url.includes('/work_result')) {
 	    next();
